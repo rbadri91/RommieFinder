7df18c2ffec005a662f27138e96f873ce7a415da
rbadri91 <rbadri01@gmail.com>
2017-06-12 17:37:26 -0400

    Changing signup page

--- config/passport.js
+++ config/passport.js
@@ -71,8 +71,6 @@ module.exports = function(passport) {
                         newUser.data.password = newUser.generateHash(password);
                         newUser.data.firstName = req.body.firstName;
                         newUser.data.lastName = req.body.lastName;
-                        newUser.data.contact = req.body.contact;
-                        newUser.data.dob = req.body.dob;
                         newUser.data.hasUpdatedProfile = false;
                         // save the user
                         newUser.save(function(err) {
